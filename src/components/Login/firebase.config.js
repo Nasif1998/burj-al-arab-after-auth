b098d2a6ad91024f6722d53a93b17f9117afba1d
Nasif123 <nasif.reza@northsouth.edu>
2021-03-27 00:11:59 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+import { configure } from "@testing-library/react";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAboxNP6RLyYHBJ44t4V31ylOF7BT6Xetg",
+    authDomain: "burj-al-arab-9b1bb.firebaseapp.com",
+    projectId: "burj-al-arab-9b1bb",
+    storageBucket: "burj-al-arab-9b1bb.appspot.com",
+    messagingSenderId: "569225377803",
+    appId: "1:569225377803:web:e71f207ef161fe2643af0e"
+  };
+  export default firebaseConfig;
